cceea5a092c3d6176b7df77dbd37f14775f9126d
Eduardo-Maia98 <dudupeloso98@hotmail.com>
2024-03-12 15:14:25 -0300

    Challange Done

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'react-native';
 import { Box, NativeBaseProvider } from 'native-base';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
@@ -6,13 +5,14 @@ import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/in
 import { THEME } from './src/theme';
 
 import Home from './src/screens/Home';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold })
   if (!fontsLoaded) { return <></> }
   return (
     <NativeBaseProvider theme={THEME}>
-
+      <StatusBar style='light'/>
       <Box flex={1} bg={'gray.700'}>
 
         <Home />
